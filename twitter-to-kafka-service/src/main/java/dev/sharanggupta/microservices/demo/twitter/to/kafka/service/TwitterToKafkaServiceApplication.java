6ade7be597a7054ce365e3a254bfb7325e48a9dd
sharang <sharanggupta96@gmail.com>
2023-01-20 10:54:12 +0000

    Adding logging

---
+++ twitter-to-kafka-service/src/main/java/dev/sharanggupta/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,37 @@
+package dev.sharanggupta.microservices.demo.twitter.to.kafka.service;
+
+import dev.sharanggupta.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+    @PostConstruct
+    public void after(){
+        LOGGER.info("App starts...");
+        LOGGER.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+        LOGGER.info("App starts...");
+        LOGGER.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+    }
+}
---
+++ twitter-to-kafka-service/src/test/java/dev/sharanggupta/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package dev.sharanggupta.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+
+}
